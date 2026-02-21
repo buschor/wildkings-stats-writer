72041f2443eab0961815383214b8d239d4832979
Ronny Buschor <rbuschor@gmail.com>
2026-02-21 17:25:41 +0100

    Initial Commit

---
+++ Wild Kings Stats Writer/Wild_Kings_Stats_WriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wild_Kings_Stats_WriterApp.swift
+//  Wild Kings Stats Writer
+//
+//  Created by Ronny Buschor on 21.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Wild_Kings_Stats_WriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
